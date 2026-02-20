c689eda7f45d3b6483c59b773993911943bde7c5
sushmitha maddela <sushmitha.maddela.ec@gmail.com>
2026-02-20 12:24:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
-from flask import Flask, request, jsonify
-import joblib
-import numpy as np
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-# Load trained model
-model = joblib.load("model.pkl")
-
-@app.route("/")
+@app.get("/")
 def home():
-    return "ML Model is Running!"
-
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.get_json()
-    value = np.array([[data["value"]]])
-    prediction = model.predict(value)
-    return jsonify({"prediction": float(prediction[0])})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    return {"message": "Railway deployment successful!"}
